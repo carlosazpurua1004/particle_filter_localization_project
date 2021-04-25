f2b00b2d1283cf10d5813eb9bba820b73dcf6e5b
Michael Linden Su <michaelsu@cloyster.cs.uchicago.edu>
2021-04-25 17:45:41 -0500

    Revert "Merge branch 'main' of https://github.com/carlosazpurua1004/particle_filter_localization_project into main"
    This reverts commit ab0afa246703e1c605ac09374b6372be14c59c75, reversing
    changes made to d2ad8e7fe18784c9d52c4213e274635b9fc0f057.

--- scripts/test_draw_sample.py
+++ scripts/test_draw_sample.py
@@ -5,8 +5,4 @@ from particle_filter import draw_random_sample
 if __name__ == "__main__":
     sampling_set = [1,2,3,4,5,6]
     samples = draw_random_sample([1,2,3,4,5,6], [.1, .3, 0.05, 0.25, 0.1, 0.2], 1000)
-    sampling_set2 = [x for x in range(1000)]
-    prob2 = [0.0005]*500 + [0.0015]*500
-    samples2 = draw_random_sample(sampling_set2, prob2, len(sampling_set2))
     print([samples.count(num) for num in sampling_set])
-    print([(num, samples2.count(num)) for num in sampling_set2])
